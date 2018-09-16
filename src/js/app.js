ce946ef855f61fef54515f960fd6ee2cd3083de0
gregmeszaros <meszaros275@gmail.com>
2018-09-16 15:28:50 +0100

    Updates wholesale fixes

--- src/js/app.js
+++ src/js/app.js
@@ -7,21 +7,30 @@ import { FiHome } from "react-icons/fi";
 import { FiShoppingBag } from "react-icons/fi";
 import { FiBriefcase } from "react-icons/fi";
 import { FiMail } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import { FiSend } from "react-icons/fi";
+
+import { IconContext } from "react-icons";
+
+import './../css/styles.css';
 
 const { render } = ReactDOM;
 
 const menuLinks = [
-  {id: 'home', link: "/", text: "Home", icon: <FiHome />},
-  {id: 'products', link: "/products", text: "Products", icon: <FiShoppingBag />},
-  {id: 'wholesale', link: "/wholesale", text: "Wholesale", icon: <FiBriefcase />},
-  {id: 'contact', link: "/contact-us", text: "Contact Us", icon: <FiMail />},
+  {id: 'home', link: "/", text: "Home", icon: <IconContext.Provider value={{ className: "nav-icon"}}><FiHome /></IconContext.Provider>},
+  {id: 'products', link: "/products", text: "Products", icon: <IconContext.Provider value={{ className: "nav-icon"}}><FiShoppingBag /></IconContext.Provider>},
+  {id: 'wholesale', link: "/wholesale", text: "Wholesale", icon: <IconContext.Provider value={{ className: "nav-icon"}}><FiBriefcase /></IconContext.Provider>},
+  {id: 'contact', link: "/contact-us", text: "Contact", icon: <IconContext.Provider value={{ className: "nav-icon"}}><FiMail /></IconContext.Provider>},
 ];
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <Logo title="Amm Zap" />
+        <Link to="/">
+          <Logo title="Amm Zap" />
+        </Link>
         <Nav links={menuLinks} />
       </header>
     );
@@ -45,8 +54,8 @@ class Nav extends React.Component {
         {this.props.links.map((menuLink) => {
           return (
             <li key={menuLink.id}>
-              <Link to= {menuLink.link}>
-                {menuLink.icon} {menuLink.text}
+              <Link to={menuLink.link}>
+                {menuLink.icon} <span className="nav-icon-text">{menuLink.text}</span>
               </Link>
             </li>
           );
@@ -70,10 +79,17 @@ class Hero extends React.Component {
       <section className="hero">
         <video key="hero-video" id="video-background" autoPlay loop muted plays-inline="true" src="https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761" type="video/mp4" />
         <h3>Welcome to ZenThai</h3>
+
         <p>ZenThai Trading Ltd. specialises in sourcing unique products from Thailand to the world’s kitchen. Our new product line is a specially selected dried fruit chilli paste which is high quality and suitable for vegans. These natural ingredients will make your dishes tastier but still remain healthy.</p>
+
         <hr />
-        <img src="assets/img/products/Apple-Chilli.png" />
-        <a href="http://tutorialzine.com/2016/06/freebie-landing-page-template-with-flexbox/" className="btn">See our product range</a>
+
+        <img src="assets/img/products/Apple-Chilli.png" width="220px" />
+
+        <Link to="/products" className="btn">
+          See our full product range
+        </Link>
+
       </section>
     );
   }
@@ -88,19 +104,25 @@ class SocialLinks extends React.Component {
         <hr />
         <ul className="grid">
           <li>
-            <i className="fa fa-camera-retro"></i>
+            <IconContext.Provider value={{ size: "2em", className: "social-instagram-custom" }}>
+              <FiInstagram />
+            </IconContext.Provider>
             <h4>Instagram</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
           </li>
 
           <li>
-            <i className="fa fa-cubes"></i>
+            <IconContext.Provider value={{ size: "2em", className: "social-fb-custom" }}>
+              <FiFacebook />
+            </IconContext.Provider>
             <h4>Facebook</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
           </li>
 
           <li>
-            <i className="fa fa-newspaper-o"></i>
+            <IconContext.Provider value={{ size: "2em", className: "social-mail-custom" }}>
+              <FiMail />
+            </IconContext.Provider>
             <h4>E-mail</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
           </li>
@@ -151,6 +173,8 @@ class ListProducts extends React.Component {
       <div className="product-list">
         <Header />
         <section className="features">
+          <h3 className="title">Our Products</h3>
+
           <p>
           Some of our nice products
           </p>
@@ -171,11 +195,40 @@ class Wholesale extends React.Component {
       <div className="wholesale">
         <Header />
         <section className="features">
+          <h3 className="title">Wholesale</h3>
+
           <p>
-          If you interested to buy products from us in bigger batch please contact us and we will come back to you.
-          <br />
-          Please see our price list!
+            If you interested to buy products from us in bigger batch please contact us and we will come back to you.
           </p>
+
+          <div>
+            <ul className="list-visible">
+              <li className="list-circle">See our chilli paste range <a href="assets/pdf/zenthai-chilli.pdf" target="_blank"><b>here</b></a>.</li>
+              <li className="list-circle">See our NEW chilli herbs range <a href="assets/pdf/zenthai-chilli-herbs.pdf" target="_blank"><b>here</b></a>.</li>
+            </ul>
+
+            <p></p>
+            <ul className="list-visible">
+              <li className="list-circle"><b>Full price list available <a href="assets/pdf/zenthai-price-list-2018.pdf" target="_blank">here</a></b></li>
+            </ul>
+
+            <p>
+              <h4>Sachet size:</h4>
+              <img src="assets/img/sachet-size.jpg" alt="chilli sprinkle sachet size" className="img-responsive" />
+            </p>
+
+            <p>
+              <h4>Sachet types</h4>
+              <img src="assets/img/packaging-new.jpg" alt="chilli sprinkle sachet packaging" className="img-responsive" />
+            </p>
+
+            <p>
+              <h4>Box size:</h4>
+              <img src="assets/img/box-size.png" alt="chilli sprinkle box size" className="img-responsive" />
+            </p>
+            
+          </div>
+
         </section>
         <SocialLinks />
         <BottomRightSticky />
@@ -205,7 +258,7 @@ class ContactPage extends React.Component {
                 <textarea rows="5" cols="50" value="Your query" />
               </p>
               <p>
-                <a href="#" className="btn">Send</a>
+                <a href="#" className="btn">Send <FiSend /></a>
               </p>
             </form>
         </section>
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -13,7 +13,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         loader: "babel-loader",
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader?url=false" }
+        ]
+      },
     ]
   }
 }
